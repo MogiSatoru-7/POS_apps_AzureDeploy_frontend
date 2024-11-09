d5fdce7672beb290c18e79c90b25bfc5c16dd359
Mogi Satoru-7 <satoru.mogi.0103@gmail.com>
2024-11-10 06:58:45 +0900

    DB参照エラー対応１

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ const Page = () => {
     try {
       // 各商品をバックエンドに送信する
       for (const item of purchases) {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}purchase`, {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/purchase`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
