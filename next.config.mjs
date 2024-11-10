a31059f6c536ea4a3d10cc05b4e34a4e5262fb6c
Mogi Satoru-7 <satoru.mogi.0103@gmail.com>
2024-11-10 11:28:31 +0900

    DB参照エラー対応５

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone'
+  output: 'standalone',
+  env: {
+      NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API, // 環境変数を追加
   }
+}
+
+export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'standalone'
+//   }
   
-  export default nextConfig;
+//   export default nextConfig;
