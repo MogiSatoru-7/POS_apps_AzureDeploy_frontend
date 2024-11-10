533f57b367e7df0a662d72451a7ba6e9d9948bbe
Mogi Satoru-7 <satoru.mogi.0103@gmail.com>
2024-11-10 10:18:47 +0900

    DB参照エラー対応４

--- src/app/page.js
+++ src/app/page.js
@@ -75,20 +75,3 @@ const Page = () => {
 
 export default Page;
 
-
-// DeployTest
-// export default function Home() {
-//   return (
-//     <div style={{
-//       display: 'flex',
-//       justifyContent: 'center',
-//       alignItems: 'center',
-//       height: '100vh',
-//       fontSize: '2rem',
-//       fontWeight: 'bold'
-//     }}>
-//       Welcome!
-//     </div>
-//   );
-// }
-
